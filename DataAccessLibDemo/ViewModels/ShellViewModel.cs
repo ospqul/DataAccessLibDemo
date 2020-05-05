1987387edc02844f139297d2a5527e64c555f590
Qu Long <long.qu@olympus-ap.com>
2020-05-05 11:01:03 +0800

    Add 2_Add_reference_and_open_file

--- DataAccessLibDemo/ViewModels/ShellViewModel.cs
+++ DataAccessLibDemo/ViewModels/ShellViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Caliburn.Micro;
+using RDTiffDataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace DataAccessLibDemo.ViewModels
 {
-    class ShellViewModel
+    class ShellViewModel : Screen, IDisposable
     {
+        public RDTiffDataFile.RDTiffDataFile dataFile { get; set; }
+
+        public ShellViewModel()
+        {
+            FilePath = @"C:\Program Files\OlympusNDT\NDT Data Access Library 1.12\Samples\x64\DATA FILE - WELD.opd";
+        }
+
+        public void OpenFile()
+        {
+            RDTiffData rdtData = new RDTiffData();
+            dataFile = rdtData.RDTiffDataFile;
+            dataFile.OpenFile(FilePath);
+        }
+
+        public void Dispose()
+        {
+            dataFile.CloseFile();
+        }
+
+        private string _filePath;
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set 
+            {
+                _filePath = value;
+                NotifyOfPropertyChange(() => FilePath);
+            }
+        }        
     }
 }
